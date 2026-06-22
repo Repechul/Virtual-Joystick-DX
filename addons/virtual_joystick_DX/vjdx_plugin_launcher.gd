9191ca89b821d002b4c710c6b3335dce9356b948
Repechul <repechuldev@gmail.com>
2026-06-22 07:19:13 -0300

    v0.3a fix

--- addons/virtual_joystick_DX/vjdx_plugin_launcher.gd
+++ addons/virtual_joystick_DX/vjdx_plugin_launcher.gd
@@ -7,11 +7,11 @@ const VIRT_JOY_DX_ICON:= preload("res://addons/virtual_joystick_DX/vjdx_icon.svg
 
 func _enter_tree():
 	add_custom_type(
-		"VirtualJoystickDeluxe",
+		"VirtualJoystickDX",
 		"control",
 		JOYSTICK_CORE_SCRIPT,
 		VIRT_JOY_DX_ICON
 		)
 
 func _exit_tree():
-	remove_custom_type("VirtualJoystickDeluxe")
+	remove_custom_type("VirtualJoystickDX")
